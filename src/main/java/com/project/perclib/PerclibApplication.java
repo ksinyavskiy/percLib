7fff9750d8ae7a8a3a4ea19e858ce39fe61bfc3b
ksinyavskiy <kirill.sinyavskiy@cengage.com>
2023-12-10 15:42:22 +0100

    initial commit

---
+++ src/main/java/com/project/perclib/PerclibApplication.java
@@ -0,0 +1,13 @@
+package com.project.perclib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerclibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerclibApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/perclib/PerclibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.perclib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerclibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
